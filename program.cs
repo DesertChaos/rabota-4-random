c308ee25babe36c7ec0d0fa7395ce6733cf391a1
DesertChaos <vadimsysolin52565@gmail.com>
2025-02-13 21:33:56 +0300

    Update program.cs

--- program.cs
+++ program.cs
@@ -4,31 +4,31 @@ namespace very_important_code_goes_here
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Попробуйте угадать число от 1 до 100");
+            Console.WriteLine("Попробуйте угадать число от 1 до 100.");
             
             Random rnd = new Random();
             int goal = rnd.Next(1, 101);
-            Console.WriteLine($"По секрету, это число - {goal}");
+            Console.WriteLine($"По секрету, это число - {goal}.");
             int counter = 0; //счетчик попыток пользователя
             int answer;
             
             start:  //оказывается можно ставить метки просто так. приколь конечно
-                Console.WriteLine($"Введите число. Номер попытки {counter + 1}" );
+                Console.WriteLine($"Введите число. Номер попытки {counter + 1}." );
                 if (!int.TryParse(Console.ReadLine(), out answer)) { goto start; } //есть проверка на дурачка
                 counter++;
 
             switch (answer)
             {
                 case int n when n == goal && counter < 5:
-                    Console.WriteLine("Верно");
+                    Console.WriteLine("Верно.");
                     break;
 
                 case int x when x != goal && counter > 4:
-                    Console.WriteLine("Попытки закончились");
+                    Console.WriteLine("Неверно. Попытки закончились");
                     break;
 
                 default:
-                    Console.WriteLine("Неверно");                   
+                    Console.WriteLine("Неверно.");                   
                     goto start;
             }
 
